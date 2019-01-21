3787ba39d6964e2cc5dd8f552a768386607dc301
Jim Speaker <Jim.Speaker@PREMERA.com>
2019-01-21 15:50:04 -0800

    jspaeker #LAB added cookie class.

---
+++ VstsProjectMonitor.Service.ClientTests/wwwroot/specs/cookie-baker-tests.js
@@ -0,0 +1,38 @@
+﻿// ReSharper disable UseOfImplicitGlobalInFunctionScope
+
+/// <reference path="../../../VstsProjectMonitor.Service/wwwroot/scripts/extensions.js" />
+/// <reference path="../../../VstsProjectMonitor.Service/wwwroot/scripts/project-monitor.js" />
+/// <reference path="../../../VstsProjectMonitor.Service/wwwroot/scripts/cookie-baker.js" />
+
+/// <reference path="../dependencies/jquery-3.3.1.min.js" />
+/// <reference path="../dependencies/js.cookie.min.js" />
+
+describe("GivenBuildDefinitions",
+  function () {
+    var cookieBaker = new ProjectMonitor.CookieBaker();
+    var cookieName = "tasty-cookie";
+    var ingredients = { name: "myProj" };
+
+    beforeEach(function() {
+      Cookies.remove(cookieName);
+    });
+
+    it("WhenAskingToBakeCookie_ThenItShouldBakeCookie",
+      function () {
+
+        cookieBaker.bake(cookieName, ingredients);
+        var cookie = Cookies.get(cookieName);
+        expect(JSON.parse(cookie).name).toBe("myProj");
+
+      });
+
+    describe("AndCookieExists",
+      function() {
+        it("WhenAskingToRetrieveCookie_ThenItShouldRetrieveCookie",
+          function() {
+            Cookies.set(cookieName, ingredients);
+            var cookie = cookieBaker.retrieve(cookieName);
+            expect(JSON.parse(cookie).name).toBe("myProj");
+          });
+      });
+  });
---
+++ VstsProjectMonitor.Service/wwwroot/scripts/cookie-baker.js
@@ -0,0 +1,22 @@
+﻿var ProjectMonitor = ProjectMonitor || {};
+
+// ReSharper disable once CyclomaticComplexity
+ProjectMonitor.CookieBaker = function (cookieRepository) {
+  // ReSharper disable CallerCalleeUsing
+  if (!cookieRepository) return new arguments.callee(Cookies);
+  if (!(this instanceof arguments.callee)) return new arguments.callee(Cookies);
+  // ReSharper restore CallerCalleeUsing
+
+  var bake = function (cookieName, ingredients) {
+    cookieRepository.set(cookieName, ingredients);
+  };
+
+  var retrieve = function(cookieName) {
+    return cookieRepository.get(cookieName);
+  };
+
+  return {
+    bake: bake,
+    retrieve: retrieve
+  };
+};
